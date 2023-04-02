fc3be7604be397043d968d8ac980ecdb7e28b1f8
simonsteiger <simon.j.steiger@gmail.com>
2023-04-02 10:12:08 +0200

    Make data clean more general

--- R/load.R
+++ R/load.R
@@ -1,26 +1,33 @@
 box::use(
   nc = ncdf4,
   ut = utils,
+  pr = purrr,
+  magrittr[`%>%`],
 )
 
+#' @export
+names <- c("nppv", "chl", "phyc")
 
 #' @export
-nc_file <- nc$nc_open("data/copernicus_nppv.nc")
+list_nc <- pr$set_names(names) %>%
+  pr$map(~ nc$nc_open(paste0("data/copernicus_", .x, ".nc")))
 
 #' @export
-latitude <- as.vector(nc_file$dim[[3]]$vals)
+latitude <- as.vector(list_nc$nppv$dim[[3]]$vals)
 
 #' @export
-longitude <- as.vector(nc_file$dim[[4]]$vals)
+longitude <- as.vector(list_nc$nppv$dim[[4]]$vals)
 
-x <- nc$ncvar_get(nc_file, nc_file$var[[1]])
-
-matrix <- apply(x, c(1, 2), mean)
-
-colnames(matrix) <- latitude
+list_var <- pr$set_names(names) %>% 
+  pr$map(~ nc$ncvar_get(list_nc[[.x]], list_nc[[.x]]$var[[1]]))
 
 #' @export
-nc_mean <- matrix
+list_matrix <- pr$set_names(names) %>% 
+  pr$map(~ apply(list_var[[.x]], c(1, 2), mean))
+
+# list_matrix[[1]] <- colnames(latitude)
+# list_matrix[[2]] <- colnames(latitude)
+# list_matrix[[3]] <- colnames(latitude)
 
 #' @export
 envs <- ut$read.csv("data/seabird_atolls_envs_10Mar.csv")
