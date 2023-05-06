91f31a68973d1aee4f39e24a33cb2295cac230bd
simonsteiger <simon.j.steiger@gmail.com>
2023-05-06 16:49:01 +0200

    load.jl: cp to DataFrames
    - nppv, chl, phyc now in DataFrame with lat and long

--- R/load.jl
+++ R/load.jl
@@ -1,12 +1,43 @@
-using NetCDF, Statistics, BenchmarkTools
+using NetCDF # read
+using Statistics, DataFrames, Compat, Pipe # wrangling
 
-var = ["nppv", "chl", "phyc"]
+# First element is file path
+# Second entry is loaded variable
+vars = [
+  ["nppv", "nppv"],
+  ["chl", "chl"],
+  ["phyc", "phyc"],
+  #=
+  name of sea surface temperature variable?
+  ["temp_1", "???"],
+  ["temp_2", "???"],
+  =#
+  ["nppv", "longitude"],
+  ["nppv", "latitude"],
+]
 
-function readcp!(var)
-  x = Dict()
-  [x[v] = NetCDF.open(string("data/copernicus_", v, ".nc"), v) for v in var];
-  [x[v] = abs.(x[v]) for v in var];
-  [x[v] = mean(x[v], dims = 4) for v in var];
-end
+# Create a dictionary to write to
+d = Dict()
 
-@benchmark $readcp!($var)
+# Read nc files
+[d[v[2]] = NetCDF.open(string("data/copernicus_", v[1], ".nc"), v[2]) for v in vars];
+
+# Calculate absolute values of all cells
+[d[v[2]] = abs.(d[v[2]]) for v in vars];
+
+# Calculate mean of array slices containing vars[2]
+[d[v[2]] = mean(d[v[2]], dims=4) for v in vars];
+
+# Drop singleton dimensions of arrays
+[d[v[2]] = dropdims(d[v[2]], dims=n) for v in vars[1:3], n in [4, 3]]
+
+iterlat = @pipe collect(Base.Iterators.repeated(d["latitude"], length(d["longitude"]))) |>
+                collect(Base.Iterators.flatten(_))
+
+test = @pipe DataFrame(d["nppv"], :auto) |>
+             insertcols(_, 1, :longitude => d["longitude"]) |>
+             stack(_, Not(:longitude)) |>
+             insertcols(_, 2, :latitude => iterlat)
+
+# Pipeline for higher-resolution velo data will be different
+# DataFrame has different dims!
