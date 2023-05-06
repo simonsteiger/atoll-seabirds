a46944a1a322d8241db71a58abd6dcf75edc5f58
basti-st <125915549+basti-st@users.noreply.github.com>
2023-05-06 20:37:33 +1200

    rda

--- R/analysis.R
+++ R/analysis.R
@@ -1,7 +1,10 @@
 box::use(
   dp = dplyr,
   tdr = tidyr,
+  tbl = tibble,
   ts = tidyselect,
+  veg = vegan,
+  gg = ggplot2
 )
 
 pop <- read.csv("data/atoll_seabird_populations_11Mar.csv")
@@ -21,3 +24,30 @@ pop_recode <- pop %>%
 joined <- dp$left_join(pop_recode, envs_jicp, by = "atoll") %>% 
   tdr$pivot_wider(names_from = "species", values_from = "presence") %>% 
   dp$select(-X)
+
+
+
+rda.env <- veg$rda(joined[,c(6:11,14,16:24)], weight = FALSE, scale = TRUE)
+
+env.scores <- veg$scores(rda.env)$sites %>% as.data.frame()
+env.scores$atoll <- joined$atoll
+env.scores$Sula_leucogaster <- joined$Sula_leucogaster
+
+hull_data <- env.scores %>% dp$group_by(Sula_leucogaster) %>% dp$slice(chull(PC1, PC2)) %>% as.data.frame()
+
+env.loadings <- veg$scores(rda.env)$species %>% as.data.frame()
+
+
+gg$ggplot(env.scores, gg$aes(x = PC1, y = PC2)) +
+  gg$geom_point(gg$aes(colour = Sula_leucogaster)) +
+  gg$geom_segment(data = env.loadings, gg$aes(x = 0, xend = PC1, y=0, yend = PC2), 
+               color = "black", arrow = gg$arrow(length = gg$unit(0.01,"npc"))) +
+  gg$geom_text(data = env.loadings, 
+            gg$aes(x = PC1, y = PC2, label = rownames(env.loadings),
+                hjust = 0.1*(1-sign(PC1)), vjust = 0.5*(1-sign(PC2)))) +
+  gg$geom_polygon(data = hull_data,
+               gg$aes(fill = Sula_leucogaster, colour = Sula_leucogaster), alpha = 0.3, show.legend = FALSE) +
+  gg$geom_hline(yintercept=0, linetype="dotted") +
+  gg$geom_vline(xintercept=0, linetype="dotted") +
+  gg$theme_classic() 
+
