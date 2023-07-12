318f0ce9938d70fa9d7d1c4b6be15c2b36e7c306
simonsteiger <simon.j.steiger@gmail.com>
2023-07-12 16:04:27 +0200

    Start jisao.jl

---
+++ jl/jisao.jl
@@ -0,0 +1,20 @@
+using NetCDF
+using Statistics
+
+vars = [
+    ["lat", "latitude"],
+    ["lon", "longitude"],
+    ["data", "precip_anom"]
+]
+
+dct = Dict()
+
+# Open NC
+[dct[v[2]] = NetCDF.open("data/preci_anom_cor.nc", v[1]) for v in vars]
+
+# Why are some numbers in dct["precip_anom"][:, :, 2] so huge?
+
+# Calculate absolute values of all cells
+[dct[v[2]] = abs.(dct[v[2]]) for v in vars];
+
+[dct[v[2]] = mean(dct[v[2]], dims=4) for v in vars];
