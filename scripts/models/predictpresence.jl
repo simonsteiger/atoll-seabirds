d1c6da44f28c3d23f9ce326a2d92537e65549034
simonsteiger <simon.j.steiger@gmail.com>
2023-10-19 10:13:01 +0200

    Reg, Spe, RxS model

--- scripts/models/predictpresence.jl
+++ scripts/models/predictpresence.jl
@@ -11,7 +11,7 @@ using StatsFuns, LinearAlgebra
 # Working with tabular data
 using Chain, DataFrames
 # Saving results
-using Serialization
+using Serialization, CSV
 
 # Load custom modules
 include("../preprocessing/preprocess.jl")
@@ -20,7 +20,7 @@ include("../../src/utilities.jl")
 include("../visualization/diagnosticplots.jl")
 include("../visualization/paramplots.jl")
 
-# Add names to environment
+# Add custom modules
 using .Preprocess
 using .Postprocess
 using .CustomUtilityFuns
@@ -28,96 +28,41 @@ using .DiagnosticPlots
 using .ParamPlots
 
 # Benchmark model?
-benchmark = false
+benchmark = true
 
 # Load a saved chain?
-load = true
+load = false
 
-# Which chain should be loaded?
-filepath = "chains/predictpresence.jls"
+# If not loading a chain, save results to path below
+savetofile = "predictpresence_T"
+
+# If loading a chain, which file should be loaded?
+loadfrompath = "chains/predictpresence.jls"
 
 ### MODEL SPECIFICATION ###
 
-@model function modelpresence(PC, r, s, n, s_in_n, Nv, Ng, Nb, y)
-    # Number of groups per predictor
-    Nr = length(unique(r))
-    Ns = length(unique(s))
-    Nn = length(unique(n))
+lu(x) = length(unique(x))
 
+@model function modelpresence(r, s, y; Nr=lu(r), Ns=lu(s), k=maximum(s))
     # Priors for region
-    λ̄ ~ Normal()
-    κ ~ Exponential(1)
-    λ ~ filldist(Normal(λ̄, κ), Nr, Ns)
+    θ_reg ~ filldist(TDist(3), Nr)
 
-    # PC1 per nesting type
-    θ̄1 ~ filldist(Normal(), Nn) # Distribution of distributions per nesting type
-    τ1 ~ filldist(Exponential(1), Nn)
-    θ11 ~ filldist(Normal(θ̄1[1], τ1[1]), Nb) # Vector of distributions of ground nesters
-    θ12 ~ filldist(Normal(θ̄1[2], τ1[2]), Ng) # Vector of distributions of ground nesters
-    θ13 ~ filldist(Normal(θ̄1[3], τ1[3]), Nv) # Vector of distributions of ground nesters
-    θ1 = [θ11, θ12, θ13]
+    # Priors for species
+    θ_spe ~ filldist(TDist(3), Ns)
 
-    # PC2 per nesting type
-    θ̄2 ~ filldist(Normal(), Nn) # Distribution of distributions per nesting type
-    τ2 ~ filldist(Exponential(1), Nn)
-    θ21 ~ filldist(Normal(θ̄2[1], τ2[1]), Nb) # Vector of distributions of ground nesters
-    θ22 ~ filldist(Normal(θ̄2[2], τ2[2]), Ng) # Vector of distributions of tree nesters
-    θ23 ~ filldist(Normal(θ̄2[3], τ2[3]), Nv) # Vector of distributions of burrow nesters
-    θ2 = [θ21, θ22, θ23]
-
-    # PC3 per nesting type
-    θ̄3 ~ filldist(Normal(), Nn) # Distribution of distributions per nesting type
-    τ3 ~ filldist(Exponential(1), Nn)
-    θ31 ~ filldist(Normal(θ̄3[1], τ3[1]), Nb) # Vector of distributions of ground nesters
-    θ32 ~ filldist(Normal(θ̄3[2], τ3[2]), Ng) # Vector of distributions of tree nesters
-    θ33 ~ filldist(Normal(θ̄3[3], τ3[3]), Nv) # Vector of distributions of burrow nesters
-    θ3 = [θ31, θ32, θ33]
-
-    # PC4 per nesting type
-    θ̄4 ~ filldist(Normal(), Nn) # Distribution of distributions per nesting type
-    τ4 ~ filldist(Exponential(1), Nn)
-    θ41 ~ filldist(Normal(θ̄4[1], τ4[1]), Nb) # Vector of distributions of ground nesters
-    θ42 ~ filldist(Normal(θ̄4[2], τ4[2]), Ng) # Vector of distributions of tree nesters
-    θ43 ~ filldist(Normal(θ̄4[3], τ4[3]), Nv) # Vector of distributions of burrow nesters
-    θ4 = [θ41, θ42, θ43]
-
-    # PC5 per nesting type
-    θ̄5 ~ filldist(Normal(), Nn) # Distribution of distributions per nesting type
-    τ5 ~ filldist(Exponential(1), Nn)
-    θ51 ~ filldist(Normal(θ̄5[1], τ5[1]), Nb) # Vector of distributions of ground nesters
-    θ52 ~ filldist(Normal(θ̄5[2], τ5[2]), Ng) # Vector of distributions of tree nesters
-    θ53 ~ filldist(Normal(θ̄5[3], τ5[3]), Nv) # Vector of distributions of burrow nesters
-    θ5 = [θ51, θ52, θ53]
-
-    # PC6 per nesting type
-    θ̄6 ~ filldist(Normal(), Nn) # Distribution of distributions per nesting type
-    τ6 ~ filldist(Exponential(1), Nn)
-    θ61 ~ filldist(Normal(θ̄6[1], τ6[1]), Nb) # Vector of distributions of ground nesters
-    θ62 ~ filldist(Normal(θ̄6[2], τ6[2]), Ng) # Vector of distributions of tree nesters
-    θ63 ~ filldist(Normal(θ̄6[3], τ6[3]), Nv) # Vector of distributions of burrow nesters
-    θ6 = [θ61, θ62, θ63]
+    # Priors for region x species
+    τ_rxs ~ Exponential(0.5)
+    θ_rxs ~ filldist(Normal(0, τ_rxs), Nr * Ns)
 
     for i in eachindex(y)
-        p = logistic(
-            λ[r[i], s[i]] +
-            θ1[n[i]][s_in_n[i]] * PC[i, 1] +
-            θ2[n[i]][s_in_n[i]] * PC[i, 2] +
-            θ3[n[i]][s_in_n[i]] * PC[i, 3] +
-            θ4[n[i]][s_in_n[i]] * PC[i, 4] +
-            θ5[n[i]][s_in_n[i]] * PC[i, 5] +
-            θ6[n[i]][s_in_n[i]] * PC[i, 6]
-        )
+        p = logistic(θ_reg[r[i]] + θ_spe[s[i]] + θ_rxs[s[i]+(r[i]-1)*k])
         y[i] ~ Bernoulli(p)
     end
 end;
 
 model = modelpresence(
-    PC,
     num_region,
     num_species,
-    num_nesting,
-    num_species_within_nesting,
-    count_species_by_nesting...,
     presence
 );
 
@@ -142,17 +87,17 @@ Turing.setrdcache(true)
 
 # Sample from model
 if load
-    chain = deserialize(filepath);
+    chain = deserialize(loadfrompath);
 else
     chain = sample(
         model,
         HMC(0.025, 10), # tuned to acceptance_rate ≈ 0.65, see https://pythonhosted.org/pyhmc/tuning.html
-        MCMCThreads(),
-        40_000,         # number of samples
-        4;              # number of chains
+        #MCMCThreads(),
+        5_000,         # number of samples
+        #1;              # number of chains
         discard_initial=2000
     )
-    serialize("chains/predictpresence.jls", chain)
+    serialize("chains/$savetofile.jls", chain)
 end
 
 # Check acceptance rate
@@ -187,9 +132,7 @@ end
 df = DataFrame([preds...])
 insertcols!(df, 1, :atoll => Preprocess.str_atoll_unknown)
 
-using CSV
-
-CSV.write("../../data/predictpresence.csv", df)
+CSV.write("../../data/$savetofile.csv", df)
 
 function specieshisto(df, idx)
     p = histogram(df[:, idx], bins=10)
