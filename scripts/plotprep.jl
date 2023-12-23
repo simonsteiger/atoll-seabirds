1ec05e8ea8a57c2eb85a3f593fb5b35d26ce329b
simonsteiger <simon.j.steiger@gmail.com>
2023-12-23 10:11:24 +0100

    Finish foraging map

--- scripts/plotprep.jl
+++ scripts/plotprep.jl
@@ -4,6 +4,35 @@ const ROOT = dirname(Base.active_project())
 
 preds = CSV.read("$ROOT/data/allpopulations.csv", DataFrame)
 envs = CSV.read("$ROOT/data/seabird_atolls_envs.csv", DataFrame)
+raw_foraging = CSV.read("$ROOT/data/seabird_atolls_foraging.csv", DataFrame)
+
+foraging = copy(raw_foraging)
+
+select!(foraging, :species, r"maximum")
+
+function addspecies(df, target, reference)
+    return vcat(df, transform(df[df.species.==reference, :], :species => (_ -> target) => identity))
+end
+
+foraging = @chain foraging begin
+    groupby(_, :species)
+    combine(_, r"max" => (x -> (mean=mean(x), std=std(x))) => AsTable)
+    transform(_, :species => ByRow(x -> occursin("Sternula_sp", x) ? "Sternula_saundersi" : x) => identity)
+    transform(_, :species => ByRow(x -> occursin("Hydrobates", x) ? "Hydrobates_tristrami" : x) => identity)
+    addspecies(_, "Nesofregetta_fuliginosa", "Hydrobates_tristrami")
+    addspecies(_, "Onychoprion_lunatus", "Onychoprion_fuscatus")
+    transform(_, :species => ByRow(x -> split(x, "_")[1]) => :genus)
+    groupby(_, :genus)
+    transform(_, [:mean, :std] => ((μ, σ) -> [isnan(s) ? std(μ) : s for s in σ]) => :std)
+    addspecies(_, "Pterodroma_hypoleuca", "Pterodroma_alba")
+    groupby(_, :genus)
+    transform(_, [:species, :mean, :std] => ((sp, μ, σ) -> [occursin("hypoleuca", sp[i]) ? (mean=mean(μ), std=std(μ)) : (mean=μ[i], std=σ[i]) for i in eachindex(sp)]) => AsTable)
+    transform(_, :genus => ByRow(x -> occursin(r"Hydroprogne|Thalasseus", x) ? "Hydroprogne/Thalasseus" : x) => identity)
+    groupby(_, :genus)
+    transform(_, [:species, :mean, :std] => ((sp, μ, σ) -> [occursin("caspia", sp[i]) ? (mean=μ[i], std=std(μ)) : (mean=μ[i], std=σ[i]) for i in eachindex(sp)]) => AsTable)
+    select(_, Not(:genus))
+end
+
 
 function rdistearth(x1::AbstractArray{Float64}, x2::AbstractArray{Float64}, R=6378.388)
     coslat1 = @. cos((x1[:, 2] * π) / 180)
@@ -27,13 +56,20 @@ km2d(km, baselat) = km ./ rdistearth([0.0 baselat], [1.0 baselat])
 
 km2d.([-1.4, -1.2], [-2.3, -2.2])
 
-preds.Δ = rand(Uniform(15, 150), nrow(preds))
-
 preds.nbirds = Int64.(round.(preds.nbirds))
+leftjoin!(preds, foraging, on=:species)
 
-distance = @chain preds [fill(Weibull(8, r.Δ), r.nbirds) for r in eachrow(_)] [rand.(W) for W in _] reduce(vcat, _)
+distance = @chain preds begin
+    [fill(truncated(Normal(r.mean, r.std)), r.nbirds) for r in eachrow(_)]
+    [rand.(W) for W in _]
+    reduce(vcat, _)
+end
 
-direction = @chain preds.nbirds [fill(Uniform(0, 360), n) for n in _] [rand.(U) for U in _] reduce(vcat, _)
+direction = @chain preds.nbirds begin
+    [fill(Uniform(0, 360), n) for n in _]
+    [rand.(U) for U in _]
+    reduce(vcat, _)
+end
 
 atolls = reduce(vcat, [fill(preds.atoll[i], preds.nbirds[i]) for i in eachindex(preds.nbirds)])
 
@@ -48,22 +84,12 @@ out.long = [long < 0 ? long + 360 : long for long in out.long]
 out.addlatdeg = vec(reduce(vcat, km2d.(out.addlatkm, out.lat)))
 out.addlongdeg = vec(reduce(vcat, km2d.(out.addlongkm, out.long)))
 
+# TODO subset to Cocos only and check if result is oval
+
 select!(out, :lat, :long, :addlatdeg => :addlat, :addlongdeg => :addlong)
 
-transform!(out, [[:lat, :addlat], [:long, :addlong]] .=> ByRow((x,y) -> round(x + y, digits=1)) .=> [:adjlat, :adjlong])
+transform!(out, [[:lat, :addlat], [:long, :addlong]] .=> ByRow((x, y) -> round(x + y, digits=1)) .=> [:adjlat, :adjlong])
 select!(out, r"adj")
-
-subset_size = round(Int, 0.1 * nrow(out))
-random_rows = sample(eachindex(out.adjlat), subset_size, replace = false)
-
-sub_out = out[random_rows, :]
-
-#out = @chain out begin
-#    groupby(_, [:adjlat, :adjlong])
-#    combine(_, nrow)
-#    transform(_, :nrow => ByRow(log) => :dens)
-#    select(_, Not(:nrow))
-#end
 
 using RCall
 
